bfd359e1236e58afe4a912c94c4cc68576e7a92c
Azis Ramadhan <azisramadhan@protonmail.com>
2022-07-16 12:19:58 +0700

    Fix animation
    - Bunch of stuffs i forgot

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,10 @@ var link_anim = anime.timeline({
     easing: 'easeOutQuad'
 });
 
+var bg_anim = anime.timeline({
+    easing: 'linear'
+});
+
 window.addEventListener('load',() => {
     h_anim.add({
         targets: ['.pfp', '.header-details > h1', '.header-details > p'],
@@ -17,10 +21,15 @@ window.addEventListener('load',() => {
         duration: 750
     })
     link_anim.add({
-        targets: '.soc-links',
-        translateY: [-20, 0],
+        targets: '.soc-links > a',
+        translateY: [20, 0],
         opacity: [0, 1],
-        delay: 1100,
+        delay: anime.stagger(150, {start: 1000}),
         duration: 700
+    })
+    bg_anim.add({
+        targets: '.background',
+        opacity: [0, 1],
+        duration: 1500
     })
 });
